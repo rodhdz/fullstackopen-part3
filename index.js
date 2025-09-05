278b9f015a3937295d94ed00ce61cc01e5053e5c
Rodrigo Hernández Burad <rhernand@tec.mx>
2025-09-04 21:05:56 -0600

    uibuild

--- index.js
+++ index.js
@@ -32,14 +32,15 @@ app.get('/info', (request, response) => {
         });
 })
 
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(person => {
-        response.json(person)
-    })
-
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+        .then(person => {
+            response.json(person)
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     let isPerson
 
@@ -87,10 +88,23 @@ app.get('/api/persons/:id', (request, response) => {
         .catch(error => { response.status(404).end() })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
         .then(person => {
             response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    const person = {
+        "name": body.name,
+        "number": body.number
+    }
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
         })
         .catch(error => next(error))
 })
